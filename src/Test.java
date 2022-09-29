163b94180416f4eae667c98344711fe3a64d5612
iissaad <97767921+iissaad@users.noreply.github.com>
2022-09-29 16:15:41 -0400

    Added Info Overview

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Test {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input something: " + scanner.next());
+
+        scanner.close();
+    }
+}
